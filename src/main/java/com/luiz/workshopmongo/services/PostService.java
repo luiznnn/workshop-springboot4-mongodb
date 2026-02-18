ea5924146450a6dacc0509547e99bc5ba826a043
Luiz Felipe <luizfelipealvesneves3@gmail.com>
2026-02-17 21:14:05 -0400

    Consulta com varios criterios

--- src/main/java/com/luiz/workshopmongo/services/PostService.java
+++ src/main/java/com/luiz/workshopmongo/services/PostService.java
@@ -1,6 +1,8 @@
 package com.luiz.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,16 +14,20 @@ import com.luiz.workshopmongo.services.exception.ObjectNotFoundException;
 @Service
 public class PostService {
 
-    @Autowired
-    private PostRepository repo;
+	@Autowired
+	private PostRepository repo;
 
-    public Post findById(String id) {
-        return repo.findById(id)
-                .orElseThrow(() ->
-                        new ObjectNotFoundException("Objeto não encontrado! Id: " + id));
-    }
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
 
-    public List<Post> findByTitle(String text) {
-        return repo.findByTitleContainingIgnoreCase(text);
-    }
+	public List<Post> findByTitle(String text) {
+		return repo.searchTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
+	}
 }
